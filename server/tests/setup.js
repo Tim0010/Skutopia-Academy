3c771053955f5105d7535a183cdfdf8528129032
Timothy <timothy.kasupa.chibinda@gmail.com>
2025-01-02 12:31:17 +0530

    Update test setup with proper MongoDB memory server configuration

--- server/tests/setup.js
+++ server/tests/setup.js
@@ -9,12 +9,27 @@ dotenv.config({ path: path.join(__dirname, '../.env.test') });
 let mongod;
 
 beforeAll(async () => {
+  // Create an instance of MongoMemoryServer
   mongod = await MongoMemoryServer.create();
   const uri = mongod.getUri();
-  await mongoose.connect(uri);
+  
+  // Connect to the in-memory database
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+beforeEach(async () => {
+  // Clear all collections before each test
+  const collections = await mongoose.connection.db.collections();
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
 });
 
 afterAll(async () => {
+  // Clean up after tests
   await mongoose.connection.close();
   await mongod.stop();
 });
